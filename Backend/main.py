5ff6f13cb232dc4b052f8f49ad2352b581645f31
kinoman <101034920+kinoman420@users.noreply.github.com>
2024-10-12 11:19:51 +0330

    yts

--- Backend/main.py
+++ Backend/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 
-from parsers import nyaasi,fitgirl,dodi,linuxtracker,torrentgalaxy, rarbg
+from parsers import nyaasi,fitgirl,dodi,linuxtracker,torrentgalaxy, rarbg2, yts
 
 
 app = FastAPI()
@@ -11,25 +11,20 @@ def search(what: str):
     nyaa_engine = nyaasi.nyaasi()
     fitgirl_engine = fitgirl.fitgirl_repacks()
     dodi_engine = dodi.dodi_repacks()
-    linux_engine = linuxtracker.linuxtracker()
-    torrentgalaxy_engine = torrentgalaxy.torrentgalaxy()    #Todo
-    rarbg_engine = rarbg.therarbg()
+    yts_engine = yts.yts_mx()
 
-    # nyaa_res = nyaa_engine.search(what)
-    # fit_res = fitgirl_engine.search(what)
-    # dodi_res = dodi_engine.search(what)
-    # linux_res = linux_engine.search(what)
-    # TorrentGalaxy_res = torrentgalaxy_engine.search(what)
-    rarbg_res = rarbg_engine.search((what))
+    nyaa_res = nyaa_engine.search(what)
+    fit_res = fitgirl_engine.search(what)
+    dodi_res = dodi_engine.search(what)
+    
+    yts_res = yts_engine.search(what)
     
     result = []
 
     # result.append(nyaa_res)
     # result.append(fit_res)
     # result.append(dodi_res)
-    # result.append(linux_res)
-    # result.append(TorrentGalaxy_res)
-    result.append(rarbg_res)
+    result.append(yts_res)
 
     
 
